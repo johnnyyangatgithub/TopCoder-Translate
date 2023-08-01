11b348f2dd43c8787a55c16716ac923d901bb2f9
Johnny <yuhangyang74@gmail.com>
2023-08-01 10:29:16 +0800

    Complete the translate app by using React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import './styles.css';
+import React, { useState } from 'react';
 import Field from './components/field';
+import Languages from './components/languages';
+import Translate from './components/translate';
 
 function App() {
+  const [language, setLanguage] = useState('es');
+  const [text, setText] = useState('');
+
   return (
-    <div>
-      <Field />
+    <div className="translate-wrapper">
+      <Field onChange={setText} />
+      <Languages language={language} onLanguageChange={setLanguage} />
+      <hr />
+      <Translate text={text} language={language} />
     </div>
   );
 }
